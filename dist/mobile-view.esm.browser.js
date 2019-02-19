c68e3b627b7c5c62dae1551690b1fa9b1d078a57
hcl2020 <907953320@qq.com>
2019-02-19 17:51:10 +0800

    build: v2.4.0

--- dist/mobile-view.esm.browser.js
+++ dist/mobile-view.esm.browser.js
@@ -1,5 +1,5 @@
 /*!
- * MobileView(m-preview) v2.3.0
+ * MobileView(m-preview) v2.4.0
  * (c) 2017-2019 HeChanglin
  * Released under the MIT License.
  */
@@ -619,143 +619,119 @@ var QRCode;
 
 var qrcode = QRCode;
 
-let strStyle =
-  '#mobile-view{padding-top:50px;text-align:center}#mobile-view-mobile{background:#333;display:inline-block;padding:3px;border-radius:20px;-webkit-box-shadow:#000 6px 6px 20px 2px;box-shadow:#666 8px 20px 26px;border:#333 1px solid}#mobile-view-mobile iframe{width:375px;height:734px;background:#fff;border:#000 2px solid;border-radius:17px;margin:0;padding:0;display:block}#mobile-view-message{position:fixed;top:0;right:0;left:0;background:rgba(204,204,204,0.5);padding:10px;color:#666;text-align:center;font-size:16px}#mobile-view-message a{color:#666}' +
-  '#mobile-view-qrcode{font-size: 14px;color: #999;text-align: center;position: fixed;top: 50%;right: 0;transform: translateY(-50%);background: #fff;padding: 10px;}';
-
-let qrcode$1;
-
+var strStyle = "#mobile-view{padding-top:50px;text-align:center}\n#mobile-view-mobile{background:#333;display:inline-block;padding:3px;border-radius:20px;-webkit-box-shadow:#000 6px 6px 20px 2px;box-shadow:#666 8px 20px 26px;border:#333 1px solid}\n#mobile-view-mobile iframe{width:375px;height:734px;background:#fff;border:#000 2px solid;border-radius:17px;margin:0;padding:0;display:block}\n#mobile-view-message{position:fixed;top:0;right:0;left:0;background:rgba(204,204,204,0.5);padding:10px;color:#666;text-align:center;font-size:16px}\n#mobile-view-message a{color:#666}\n#mobile-view-qrcode{font-size: 14px;color: #999;text-align: center;position: fixed;top: 50%;right: 0;transform: translateY(-50%);background: #fff;padding: 10px;}";
+// TODO: 浏览器兼容
+var qrcode$1;
 function makeQrCode(text) {
-  console.log('changeTo:', text);
-  if (text.match(/^http(s)?:\/\//)) {
-    text += text.match(/\?/) ? '&' : '?';
-    text += 'from=QrCode';
-  }
-
-  if (qrcode$1) {
-    qrcode$1.clear(); // clear the code.
-    qrcode$1.makeCode(text); // make another code.
-  } else {
-    qrcode$1 = new qrcode(document.getElementById('mobile-view-qrcode-img'), {
-      text: text,
-      width: 128,
-      height: 128,
-      colorDark: '#000000',
-      colorLight: '#ffffff'
-      // correctLevel: QRCode.CorrectLevel.H
-    });
-  }
+    console.log('changeTo:', text);
+    if (text.match(/^http(s)?:\/\//)) {
+        text += text.match(/\?/) ? '&' : '?';
+        text += 'from=QrCode';
+    }
+    if (qrcode$1) {
+        qrcode$1.clear(); // clear the code.
+        qrcode$1.makeCode(text); // make another code.
+    }
+    else {
+        qrcode$1 = new qrcode(document.getElementById('mobile-view-qrcode-img'), {
+            text: text,
+            width: 128,
+            height: 128,
+            colorDark: '#000000',
+            colorLight: '#ffffff'
+            // correctLevel: QRCode.CorrectLevel.H
+        });
+    }
 }
-
-let isDOMContentLoaded = function() {
-  return !!document.body;
-};
-
-let defaultOpt = {
-  message: '建议使用手机访问此页面',
-  tips: '扫描二维码用手机查看~',
-  threshold: 800 // maxWidth: 800
-};
-
-let MobileView = function MobileView(opt) {
-  opt = opt || {};
-  let message = opt.message || defaultOpt.message;
-  let tips = opt.tips || defaultOpt.tips;
-  let threshold = opt.threshold || defaultOpt.threshold;
-  if (window.innerWidth <= threshold || window.screen.width <= threshold) {
-    return false;
-  }
-
-  // document.write('<hr>')
-
-  if (!isDOMContentLoaded()) {
-    setTimeout(MobileView, 25, opt);
-    return true;
-  }
-
-  let pageUrl = location.href;
-  // let strTpl = '<div id="mobile-view"><div id="mobile-view-mobile"><p><mark></mark></p><iframe src="' +
-  let strTpl =
-    '<div id="mobile-view"><div id="mobile-view-mobile"><iframe src="' +
-    pageUrl +
-    // '"></iframe><span></span></div><div id="mobile-view-message">' +
-    '"></iframe></div><div id="mobile-view-message">' +
-    message +
-    '</div><div id="mobile-view-qrcode"><div id="mobile-view-qrcode-img"></div><p>' +
-    tips +
-    '</p></div></div><style>' +
-    strStyle +
-    '</style>';
-
-  let $body = document.body;
-  $body.innerHTML = strTpl;
-  document.head.innerHTML = '';
-
-  let $iframe = document.getElementsByTagName('iframe')[0];
-  $iframe.onload = function() {
-    let _window = $iframe.contentWindow;
-    /* 处理滚动条 */
-    let strCss =
-      '::-webkit-scrollbar{width:6px;height:5px;background-color:rgba(0,0,0,0.05)}' +
-      '::-webkit-scrollbar-thumb{border-radius:3px;background-color:rgba(0,0,0,0.3)}' +
-      '::-webkit-scrollbar-thumb:hover{border-radius:3px;background-color:rgba(0,0,0,0.7)}';
-    let $style = _window.document.createElement('style');
-    $style.innerHTML = strCss;
-    _window.document.head.appendChild($style);
-    /* 处理地址栏 */
-    let pathname = _window.location.pathname;
-
-    makeQrCode(_window.location.href);
-
-    history.pushState(null, _window.document.title, pathname);
-    _window.addEventListener('hashchange', function(Event) {
-      window.location.hash = _window.location.hash;
-    });
-
-    _window.addEventListener('popstate', function(Event) {
-      makeQrCode(_window.location.href);
-    });
-
-    let _replaceState = _window.history.replaceState;
-    if (_replaceState) {
-      _window.history.replaceState = function() {
-        _replaceState.apply(_window.history, arguments);
-        history.replaceState.apply(history, arguments);
-        makeQrCode(_window.location.href);
-      };
+function stopLoadNextEl() {
+    try {
+        window.stop();
     }
-    let _pushState = _window.history.pushState;
-    if (_pushState) {
-      _window.history.pushState = function() {
-        _pushState.apply(_window.history, arguments);
-        history.replaceState.apply(history, arguments);
-        makeQrCode(_window.location.href);
-      };
+    catch (error) {
+        // For IE
+        document.execCommand('stop');
     }
-  };
-
-  // 移除 head 中的 stylesheet
-  let $head = document.head;
-  let $heads = $head.children;
-  for (let i = 0; $heads[i]; ) {
-    if (
-      $heads[i]['type'] == 'text/css' ||
-      $heads[i]['rel'] == 'stylesheet' ||
-      $heads[i]['type'] == 'text/javascript' ||
-      $heads[i]['tagName'] == 'SCRIPT'
-    ) {
-      $head.removeChild($heads[i]);
-    } else {
-      i++;
+}
+var MobileView = function MobileView(option) {
+    if (option === void 0) { option = {}; }
+    var _a = option.tips, tips = _a === void 0 ? '扫描二维码用手机查看~' : _a, _b = option.message, message = _b === void 0 ? '建议使用手机访问此页面' : _b, _c = option.threshold, threshold = _c === void 0 ? 981 : _c, _d = option.noThrowError, noThrowError = _d === void 0 ? false : _d;
+    if (window.innerWidth <= threshold || window.screen.width <= threshold) {
+        return false;
     }
-  }
-
-  // window.onerror = function(e) {
-  // console.log(e);
-  // }
-  // throw new Error('MobileView');
-
-  return true;
+    var _e = navigator.userAgent, userAgent = _e === void 0 ? '' : _e;
+    if (!userAgent.match(/chrome/i) || userAgent.match(/edge/i)) {
+        // 暂时只支持Chrome浏览器
+        return false;
+    }
+    var bodyTpl = "\n<div id=\"mobile-view\">\n  <div id=\"mobile-view-mobile\">\n    <iframe src=\"" + location.href + "\"></iframe>\n  </div>\n  <div id=\"mobile-view-message\">" + message + "</div>\n  <div id=\"mobile-view-qrcode\">\n    <div id=\"mobile-view-qrcode-img\"></div>\n    <p>" + tips + "</p>\n  </div>\n</div>\n<style>" + strStyle + "</style>";
+    // 停止网页解析和资源加载，构造body对象 (only Chrome)
+    stopLoadNextEl();
+    document.open();
+    document.close();
+    // 移除 head 中的各种 (For Chrome 36...)
+    document.head.innerHTML = '';
+    var $body = document.body;
+    if ($body) {
+        $body.innerHTML = bodyTpl;
+    }
+    else {
+        console.warn('no body');
+        document.open();
+        document.write(bodyTpl);
+        document.close();
+    }
+    function initIframe() {
+        var contentWindow = this.contentWindow;
+        /* 处理滚动条 */
+        var strCss = '::-webkit-scrollbar{width:6px;height:5px;background-color:rgba(0,0,0,0.05)}' +
+            '::-webkit-scrollbar-thumb{border-radius:3px;background-color:rgba(0,0,0,0.3)}' +
+            '::-webkit-scrollbar-thumb:hover{border-radius:3px;background-color:rgba(0,0,0,0.7)}';
+        var $style = contentWindow.document.createElement('style');
+        $style.innerHTML = strCss;
+        contentWindow.document.head.appendChild($style);
+        /* 处理地址栏 */
+        var pathname = contentWindow.location.pathname;
+        makeQrCode(contentWindow.location.href);
+        history.pushState(null, contentWindow.document.title, pathname);
+        contentWindow.addEventListener('hashchange', function (Event) {
+            window.location.hash = contentWindow.location.hash;
+        });
+        contentWindow.addEventListener('popstate', function (Event) {
+            makeQrCode(contentWindow.location.href);
+        });
+        var _replaceState = contentWindow.history.replaceState;
+        if (_replaceState) {
+            contentWindow.history.replaceState = function () {
+                _replaceState.apply(contentWindow.history, arguments);
+                history.replaceState.apply(history, arguments);
+                makeQrCode(contentWindow.location.href);
+            };
+        }
+        var _pushState = contentWindow.history.pushState;
+        if (_pushState) {
+            contentWindow.history.pushState = function () {
+                _pushState.apply(contentWindow.history, arguments);
+                history.replaceState.apply(history, arguments);
+                makeQrCode(contentWindow.location.href);
+            };
+        }
+    }
+    var $iframe = document.getElementsByTagName('iframe')[0];
+    if ($iframe) {
+        $iframe.onload = initIframe;
+    }
+    if (noThrowError) {
+        return true;
+    }
+    else {
+        var onerror_1 = window.onerror;
+        window.onerror = function mv_onerror(message) {
+            console.log('MobileView: v2.4.0');
+            window.onerror = onerror_1;
+            return true;
+        };
+        throw new Error('MobileView：阻止后续代码执行');
+    }
 };
 
 export default MobileView;
